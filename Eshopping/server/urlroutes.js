40e420a1da2bf20f06508334392b8fa087334930
Suresh Babu. Patimeeda <spatimeeda@APPSHARK.IN>
2018-07-10 11:59:40 +0530

    Removed test URLs

--- Eshopping/server/urlroutes.js
+++ Eshopping/server/urlroutes.js
@@ -14,33 +14,7 @@ var transacter = require('../transactor');
  */
 
 var apis = {
-    
-    
-    '/ofk/hduser/register': {
-        methods: {
-            CREATE: true,
-            UPDATE: true
-        },
-        description: {
-            CREATE: 'is a HTTP POST route, handles the user registration',
-            UPDATE: 'is a HTTP PUT route, handles the user registration'
-        },
-        Validator: validator.HDProfileUserValidator,
-        Enforcer: enforcer.HDProfileUserEnforcer,
-        Transactor: transacter.HDProfileUserTransactor
-    },
     '/ofk/client/verify': {
-        methods: {
-            READ: true
-        },
-        description: {
-            READ: 'is a HTTP GET route, handles the client verification'
-        },
-        Validator: validator.OauthValidator,
-        Enforcer: enforcer.OauthEnforcer,
-        Transactor: transacter.OauthTransactor
-    },
-    '/ofk/localclient/verify': {
         methods: {
             READ: true
         },
@@ -57,17 +31,6 @@ var apis = {
         },
         description: {
             CREATE: 'is a HTTP POST route, handles the user registration'
-        },
-        Validator: validator.UserProfileValidator,
-        Enforcer: enforcer.UserProfileEnforcer,
-        Transactor: transacter.UserProfileTransactor
-    },
-    '/ofk/user/update': {
-        methods: {
-            UPDATE: true
-        },
-        description: {
-            UPDATE: 'is a HTTP UPDATE route, to update user profile'
         },
         Validator: validator.UserProfileValidator,
         Enforcer: enforcer.UserProfileEnforcer,
@@ -111,54 +74,6 @@ var apis = {
         Transactor: transacter.UserLoginTransactor
     },
     
-    '/ofk/file/upload': {
-        methods: {
-            CREATE: true,
-            READ: true
-        },
-        description: {
-            CREATE: 'is a HTTP POST route, handles the file upload',
-            READ: 'is a HTTP GET route, returns the uploaded files'
-        },
-        Validator: validator.FileUploadValidator,
-        Enforcer: enforcer.FileUploadEnforcer,
-        Transactor: transacter.FileUploadTransactor
-    },
-    '/user/password': {
-        methods: {
-            UPDATE: true
-        },
-        description: {
-            UPDATE: 'is a HTTP UPDATE route, to change the user password'
-        },
-        Validator: validator.UserPasswordValidator,
-        Enforcer: enforcer.UserPasswordEnforcer,
-        Transactor: transacter.UserPasswordTransactor
-    },
-    '/ofk/password/forgot': {
-        methods: {
-            CREATE: true,
-            READ: true
-        },
-        description: {
-            CREATE: 'To handle forgot password',
-            READ: 'To handle forgot password Reset Password'
-        },
-        Validator: validator.UserForgotPasswordValidator,
-        Enforcer: enforcer.UserForgotPasswordEnforcer,
-        Transactor: transacter.UserForgotPasswordTransactor
-    },
-    '/ofk/user/updateuserprofile': {
-        methods: {
-            UPDATE: true
-        },
-        description: {
-            UPDATE: 'is a HTTP UPDATE route, to change the user password or user email'
-        },
-        Validator: validator.UpdateUserProfileValidator,
-        Enforcer: enforcer.UpdateUserProfileEnforcer,
-        Transactor: transacter.UpdateUserProfileTransactor
-    },
     '/ofk/user/products': {
         methods: {
             UPDATE: true,
